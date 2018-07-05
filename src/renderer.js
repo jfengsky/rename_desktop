98d9cf1bb15095ac1f1885f196388987f43352ac
jiang feng <jiangfeng@jiangdeMacBook-Pro.local>
2018-07-05 15:21:33 +0800

    add typescript

---
+++ src/renderer.js
@@ -0,0 +1,2 @@
+//
+//# sourceMappingURL=renderer.js.map
---
+++ src/test.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var b_1 = require("./b");
+var a = 'aaa';
+b_1.default();
+//# sourceMappingURL=test.js.map
